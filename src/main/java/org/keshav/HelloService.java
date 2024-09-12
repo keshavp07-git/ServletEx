d3b79f3e602d65a79f2e1aecf4a2f92e6665ff02
keshavp07-git <keshav.prajapati830@gmail.com>
2024-09-12 20:39:03 +0530

    send_data after recieving request

--- src/main/java/org/keshav/HelloService.java
+++ src/main/java/org/keshav/HelloService.java
@@ -3,10 +3,17 @@ package org.keshav;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HelloService extends HttpServlet {
 
-    public void service(HttpServletRequest req , HttpServletResponse res){
+    public void doGet(HttpServletRequest req , HttpServletResponse res) throws IOException {
         System.out.println("In Service");
+        res.setContentType("text/html");
+//        res.getWriter().println("<h1>Hello World</h1>");
+//        it is a direct method
+        PrintWriter out = res.getWriter();
+        out.println("<h1>Hello World</h1>");
     }
 }
